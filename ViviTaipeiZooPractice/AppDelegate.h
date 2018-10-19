bf847af41c7f925c71967d96bc72acd300c32197
Vivi <viviyang@cathayholdings.com.tw>
2018-10-19 14:17:50 +0800

    Initial Commit

---
+++ ViviTaipeiZooPractice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ViviTaipeiZooPractice
+//
+//  Created by Vivi on 2018/10/19.
+//  Copyright © 2018 Vivi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViviTaipeiZooPractice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ViviTaipeiZooPractice
+//
+//  Created by Vivi on 2018/10/19.
+//  Copyright © 2018 Vivi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
